4bdb6f11ae41d3f3ffb920cf4562861ecb3667eb
Ben Taft <ben.taft@career2cradle.org>
2021-10-29 13:36:34 -0700

    working through the tutorial chapter of r-pkgs. improved parse_acs_table_variable()

--- R/parse_acs_table_variable.R
+++ R/parse_acs_table_variable.R
@@ -1,8 +1,23 @@
-parse_acs_table_variable <- function(.x, .y) {
+#' Break an ACS "Variable" value into its three components.
+#'
+#' @param .x tibble
+#' @param col character
+#'
+#' @return tibble
+#' @export
+#' @examples
+#' parse_acs_table_variable(tibble(Variable = "ABC123_456E"))
+#' #> # A tibble: 1 x 3
+#' #>   Table    Row `Statistic Type`
+#' #>   <chr>  <int> <chr>
+#' #> 1 ABC123   456 E
+
+parse_acs_table_variable <- function(.x, col = "Variable") {
+    require(tidyverse)
     ACS_TABLE_REGEX <- "([[:alnum:]]+)_([[:digit:]]+)(.*)"
 
     .x %>%
-        extract(Variable,
+        extract(col,
                 c("Table",
                   "Row",
                   "Statistic Type"),

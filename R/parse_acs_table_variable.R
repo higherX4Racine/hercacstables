b124ff829dec81f412f55ba3b89c4e8a9532a087
Ben Taft <ben.taft@career2cradle.org>
2021-10-29 14:11:56 -0700

    removed an unnecessary pipe from a function and updated package dependencies

--- R/parse_acs_table_variable.R
+++ R/parse_acs_table_variable.R
@@ -6,21 +6,20 @@
 #' @return tibble
 #' @export
 #' @examples
-#' parse_acs_table_variable(tibble(Variable = "ABC123_456E"))
+#' parse_acs_table_variable(dplyr::tibble(Variable = "ABC123_456E"))
 #' #> # A tibble: 1 x 3
 #' #>   Table    Row `Statistic Type`
 #' #>   <chr>  <int> <chr>
 #' #> 1 ABC123   456 E
 
 parse_acs_table_variable <- function(.x, col = "Variable") {
-    require(tidyverse)
     ACS_TABLE_REGEX <- "([[:alnum:]]+)_([[:digit:]]+)(.*)"
 
-    .x %>%
-        extract(col,
-                c("Table",
-                  "Row",
-                  "Statistic Type"),
-                ACS_TABLE_REGEX,
-                convert = TRUE)
+    tidyr::extract(.x,
+                   col,
+                   c("Table",
+                     "Row",
+                     "Statistic Type"),
+                   ACS_TABLE_REGEX,
+                   convert = TRUE)
 }
--- tests/testthat/test-parse_acs_table_variable.R
+++ tests/testthat/test-parse_acs_table_variable.R
@@ -1,8 +1,8 @@
 test_that("a 1x1 tibble parses into a 1x3 tibble", {
-    foo <- tibble(Variable = "A12345_678E")
+    foo <- dplyr::tibble(Variable = "A12345_678E")
     bar <- parse_acs_table_variable(foo)
     expect_identical(bar,
-                     tibble(
+                     dplyr::tibble(
                          Table = "A12345",
                          Row = as.integer(678),
                          `Statistic Type` = "E"
